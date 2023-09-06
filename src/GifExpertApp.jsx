8ec8c1b0968ba7503b20a4173549a0d31c533e1c
Fernando <jesusfgm45@gmail.com>
2023-09-06 10:34:27 -0600

    Fetch API - Obtener las imágenes deseadas

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,7 +3,7 @@ import { AddCategory } from './components/AddCategory';
 import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
-    const [categories, setCategories] = useState([ 'One punch', 'League of Legends' ]);
+    const [categories, setCategories] = useState(['League of Legends']);
 
     const onAddCategory = (newCategory) => {
         if(categories.includes(newCategory)) return;
