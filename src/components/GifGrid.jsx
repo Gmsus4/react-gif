1976902a88857f333fc0fadf772937ed15400701
Fernando <jesusfgm45@gmail.com>
2023-09-06 20:08:10 -0600

    Mostrar los títulos de las imágenes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -2,14 +2,27 @@ import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({category}) => {
+    const [images, setImages] = useState([]);
+
+    const getImages = async () => {
+        const newImages = await getGifs( category );
+        setImages(newImages);
+    }
+
     useEffect(() => {
-        getGifs(category);
-    }, []) //Si dejo las dependencias vacías, significa que el hook solo se va a disparar la primera vez que se crea y se construye este componente
+        getImages();
+    }, []) 
 
     return (
         <>
             <h3>{category}</h3>
-            <p>Hola mundo</p>
+            <ol>
+                {
+                    images.map((img) => (
+                        <li key={img.id}>{img.title}</li>
+                    ))
+                }
+            </ol>
         </>
     )
 }
